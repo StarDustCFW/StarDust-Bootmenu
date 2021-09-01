36ad80e8b60496e0fe42e7bcd284ddffd304e9be
PWall <34860495+PWall2222@users.noreply.github.com>
2021-09-01 14:55:47 +0200

    Moved declarations to header file

--- src/menu/gui/gui_argon_menu.c
+++ src/menu/gui/gui_argon_menu.c
@@ -43,32 +43,6 @@
 
 extern char Sversion[4];
 
-int AThemes_list(gui_menu_t *menu, u32 gridX, u32 gridY);
-void serv_display(gui_menu_t *menu, char *titleid, char *name);
-static int tool_extr_rSD(void *param);
-static int tool_reboot_rcm(void *param);
-static int tool_power_off(void *param);
-static int tool_menu_rem(void *param);
-static int tool_theme(char *param);
-static int tool_emu(u32 param);
-static int tool_Menus(u32 param);
-void tool_servises_on(char *title);
-void tool_servises_off(char *title);
-void tool_Themes_on(char *cfw);
-void tool_Themes_off(char *cfw);
-int memloader(u32 fil);
-int Incognito(char *order);
-int Autoboot(u32 fil);
-int uLaunch(u32 fil);
-void HBhide(char *folder);
-void serv_CFW(int cfw);
-int bat_show(u32 percent);
-void Ajbrillo(u32 tipo);
-void hekateOFW(u32 tipo);
-void medislay(char *flags);
-int static_menu_elements(gui_menu_t *menu);
-void pre_load_menus(int menus, bool StarUp);
-
 u32 brillo = 100;
 u32 brilloM = 50;
 
